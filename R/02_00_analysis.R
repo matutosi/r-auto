be0f97ef0757eda50a7c63c25065135dcf6b0545
Toshikazu Matsumura <matutosi@gmail.com>
2024-09-09 01:54:20 +0900

    update

--- R/02_00_analysis.R
+++ R/02_00_analysis.R
@@ -188,7 +188,7 @@ answer |> # 複数回答・クロス集計
 
   # 列の順序変更と列名の変更
   # 02_29_analysis-dplyr-others.R
-dplyr::relocate(answer, ans) # 出力は省略
+dplyr::relocate(answer, ans)   # 出力は省略
 dplyr::rename(df, ans_id = id) # 出力は省略
 
   # 個数を数えるショートカット
--- R/02_29_analysis-dplyr-others.R
+++ R/02_29_analysis-dplyr-others.R
@@ -1,5 +1,5 @@
   # 列の順序変更と列名の変更
   # 02_29_analysis-dplyr-others.R
-dplyr::relocate(answer, ans) # 出力は省略
+dplyr::relocate(answer, ans)   # 出力は省略
 dplyr::rename(df, ans_id = id) # 出力は省略
 
