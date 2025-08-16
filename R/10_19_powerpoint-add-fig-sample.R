1bb8803430da72732fa89e5fd2a479569b15d083
Toshikazu Matsumura <matutosi@gmail.com>
2025-08-17 03:58:08 +0900

    Fix syntax error in path assignment for slide2.pptx in add_fig sample script

--- R/10_19_powerpoint-add-fig-sample.R
+++ R/10_19_powerpoint-add-fig-sample.R
@@ -5,7 +5,7 @@ images <- path_images               # extract_pp_images()で抽出した画像
 pp <- reduce2(titles, images,       # タイトルと画像はスライドで異なる
               add_fig, .init = pp,
               fig_full_size = TRUE) # 以下は全スライドで同じ)
-path <- fs::path_temp("slide2.pptx")
+path <- fs::path_temp("slide2.pptx"
 print(pp, target = path)
   # shell.exec(path)
 
