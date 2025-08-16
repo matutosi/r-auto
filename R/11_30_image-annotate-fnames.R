0f2437ab15bae67ac3ac2e15949eef6ccc3f8f95
Toshikazu Matsumura <matutosi@gmail.com>
2025-08-17 03:59:29 +0900

    Add image annotation functionality to combine filenames in plots

--- R/11_30_image-annotate-fnames.R
+++ R/11_30_image-annotate-fnames.R
@@ -1,7 +1,7 @@
   # 条件を満たすファイルにファイル名を書き込んで結合する
   # 11_30_image-annotate-fnames.R
 dir <- fs::path_temp()
-regexp <- "/r_\\d+\\.(png|jpg)$"
-img_all <- image_annotate_fnames(dir = dir, regexp = regexp, ncol = 8) # ファイル名の絞り込み
+regexp <- "/r_\\d+\\.(png|jpg)$" # ファイル名の絞り込み
+img_all <- image_annotate_fnames(dir = dir, regexp = regexp, ncol = 8)
 plot(img_all)
 
